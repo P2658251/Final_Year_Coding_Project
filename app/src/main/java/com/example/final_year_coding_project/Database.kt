1a520fcfc06b5f3c567cf06e39442684a705bd70
edwardcooper <edward.cooper2002@gmail.com>
2025-03-19 18:19:40 +0000

    Setting up project, Creating Film view activity and adding database

---
+++ app/src/main/java/com/example/final_year_coding_project/Database.kt
@@ -0,0 +1,11 @@
+package com.example.final_year_coding_project
+
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+
+class Database {
+    private var database: DatabaseReference = Firebase.database.getReference()
+
+
+}
