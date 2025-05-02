06177530a489f162540c14143d3a73504d6a4977
edwardcooper <edward.cooper2002@gmail.com>
2025-05-02 01:45:39 +0100

    Adding a title to the activity to make the page look nicer

--- app/src/main/java/com/example/final_year_coding_project/ReviewsViewsActivity.kt
+++ app/src/main/java/com/example/final_year_coding_project/ReviewsViewsActivity.kt
@@ -3,14 +3,20 @@ package com.example.final_year_coding_project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -45,10 +51,30 @@ class ReviewsViewsActivity : ComponentActivity() {
                 reviews.addAll(fetchedFilms)
             }
         }
-
-        LazyColumn {
-            items(reviews) {review ->
-                ReviewItem(review)
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            Row(
+                modifier = Modifier
+                    .background(Color.DarkGray)
+                    .fillMaxWidth(), horizontalArrangement = Arrangement.Center
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = "Reviews",
+                        modifier = Modifier.padding(top = 20.dp),
+                        color = Color.White,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 30.sp
+                    )
+                }
+            }
+            LazyColumn {
+                items(reviews) { review ->
+                    ReviewItem(review)
+                }
             }
         }
     }
